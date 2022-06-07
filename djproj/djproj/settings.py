3954b9b4d7edda0a9777cf9d54694dac7b7f17e9
mohy-dev <mohy.dev@gmail.com>
2022-06-07 23:45:04 +0200

    v1.0

--- djproj/djproj/settings.py
+++ djproj/djproj/settings.py
@@ -57,7 +57,7 @@ import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\mohya\Desktop\Django\djproj\djproj\templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
